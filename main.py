7520d07906db41049d0aba20ce0cec5b1dd8c75a
PetrKavan31 <petrkavan31@gmail.com>
2024-04-28 17:38:19 +0200

    new file

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # priklad z prezentace + foto + zkusit copilota a porovnat
 class Pet:
-    def __init__(self, name, characteristic):
+    def __init__(self, name, sound, characteristic):
         self.name = name
+        self.sound = sound
         self.characteristic = characteristic
 
     def sound(self):
@@ -17,30 +18,15 @@ class Pet:
 
 
 class Dog(Pet):
-    count = 0
+    _count = 0
     def __init__(self, name, characteristic):
         super().__init__(name, characteristic)
-        Dog.count += 1
+        Dog._count += 1
 
-class Cat(Pet):
-    def __init__(self, name, characteristic):
-        super().__init__(name, characteristic)
-
-
-class Parrot(Pet):
-    def __init__(self, name, characteristic):
-        super().__init__(name, characteristic)
-
-
-class Hamster(Pet):
-    def __init__(self, name, characteristic):
-        super().__init__(name, characteristic)
 
 
 dog1 = Dog("Zeryk", "Hafff")
-cat1 = Cat("Micka", "Mnau")
-parrot1 = Parrot("Karel", "Grrrrrrrh")
-Hamster1 = Hamster("Pepa", "Chrrrrrrr")
+
 
 dog1.sound()
 dog1.show()
